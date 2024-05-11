139bbea51b082f59c41cd04a4784b703422d7707
IgnaPaz <164792786+IgnaPaz@users.noreply.github.com>
2024-05-10 23:45:55 -0400

    Delete I&E_Revista directory

--- I&E_Revista/js/carrito.js
+++
@@ -1,8 +0,0 @@
-// carrito.js
-
-function agregarAlCarrito(nombre, precio) {
-    var carrito = document.getElementById('carrito');
-    var elemento = document.createElement('div');
-    elemento.textContent = nombre + ' - $' + precio;
-    carrito.appendChild(elemento);
-}
